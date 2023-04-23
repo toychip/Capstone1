7cf8e18236cc6ad82676d3cfd763824b4919bedd
toychip <dlawnsgud427@naver.com>
2023-04-23 23:23:00 +0900

    04232330_UseCookie

--- src/main/java/com/ttt/capstone/config/AuthResolver.java
+++ src/main/java/com/ttt/capstone/config/AuthResolver.java
@@ -5,15 +5,19 @@ import com.ttt.capstone.domian.Session;
 import com.ttt.capstone.exception.Unauthorized;
 import com.ttt.capstone.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @RequiredArgsConstructor
+@Slf4j
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
     private final SessionRepository sessionRepository;
@@ -26,13 +30,24 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 
-        String accesToken = webRequest.getHeader("Authorization");
-        if (accesToken == null||accesToken.equals("")){
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null){
+            log.info("servletRequest null");
+            throw new Unauthorized();
+
+        }
+
+        Cookie[] cookies = servletRequest.getCookies();
+
+        if (cookies.length == 0){
+            log.info("키가 null");
             throw new Unauthorized();
         }
 
+        String accessToken = cookies[0].getValue();
+
         // 값이 있다면
-        Session session = sessionRepository.findByAccessToken(accesToken)
+        Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(Unauthorized::new);
 
         // db 사용자 확인 작업 추가 예정
--- src/main/java/com/ttt/capstone/controller/AuthController.java
+++ src/main/java/com/ttt/capstone/controller/AuthController.java
@@ -6,9 +6,14 @@ import com.ttt.capstone.response.SessionResponse;
 import com.ttt.capstone.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +23,20 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login){
+    public ResponseEntity<Object> login(@RequestBody Login login){
         String accessToken = authService.signin(login);
-        return new SessionResponse(accessToken);
+//        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost")    // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))   // 한 달
+                .sameSite("Strict")
+                .build();
+        log.info(">>>>>>>>>>>>> cookie={}", cookie.toString());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
